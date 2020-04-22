6605d59041451235935c0f9f7dc86029e9d1f3f0
Rodrigo Hervas <38118239+rodrigohervas@users.noreply.github.com>
2020-04-22 19:03:52 -0400

    Commented Server

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const app = require('./src/app');
 const { PORT } = require('./src/config')
 
+/** 
+ * Binds server to PORT and listens requests on the specified host and port.
+ */
 app.listen(PORT, () => {
     console.log(`Server listening at port: ${PORT}`)
 })
