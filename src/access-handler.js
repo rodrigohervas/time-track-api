8a1f36150b21238814d5b3e806703417eb4ddc21
Rodrigo Hervas <38118239+rodrigohervas@users.noreply.github.com>
2020-04-22 20:47:46 -0400

    Commented access-handler

--- src/access-handler.js
+++ src/access-handler.js
@@ -1,15 +1,20 @@
 const config = require('./config')
 const logger = require('./logger')
 
-//SECURITY HANDLE MIDDLEWARE
+/**
+ * Access handler middleware: validates that the api key is present and valid for each request
+ * @param {object} req 
+ * @param {object} res 
+ * @param {function} next 
+ */
 function accessHandler(req, res, next) {
     const apiKey = req.get('authorization')
 
     //avoid apiKey on home ('/') or about ('/about') paths
     if(req.path === '/' || req.path === '/about' || req.path === '/favicon.ico') {
         next()
-    }
-    else if (!apiKey || apiKey.split(' ')[1] !== config.API_KEY) { //mandatory apiKey for the rest of the paths
+    }//mandatory apiKey for the rest of the paths
+    else if (!apiKey || apiKey.split(' ')[1] !== config.API_KEY) {
         logger.error(`Unauthorized request to path: ${req.path}`)
         next({ status: '401', message: 'Unauthorized request' })
     }
