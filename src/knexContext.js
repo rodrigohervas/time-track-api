09a01d61bbde973687632defdbcad1ca1044c3bf
Rodrigo Hervas <38118239+rodrigohervas@users.noreply.github.com>
2020-04-22 20:50:22 -0400

    Commented knexContext

--- src/knexContext.js
+++ src/knexContext.js
@@ -1,6 +1,9 @@
 const knex = require('knex')
 const config = require('./config')
 
+/**
+ * db: database connection to PostgreSQL using knex and pg
+ */
 const db = knex({
     client: 'pg', 
     connection: (config.NODE_ENV === 'test') ? config.TEST_DATABASE_URL : config.DATABASE_URL
