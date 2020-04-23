06314b69bbac78688a75e3f396e29d1b17efa320
Rodrigo Hervas <38118239+rodrigohervas@users.noreply.github.com>
2020-04-22 20:51:48 -0400

    Commented logger

--- src/logger.js
+++ src/logger.js
@@ -1,5 +1,10 @@
 const winston = require('winston')
 
+/**
+ * logger: logging middleware for errors using winston
+ */
+
+//logger confirguration
 const logger = winston.createLogger({
     level: 'info', 
     format: winston.format.json(),
@@ -7,6 +12,7 @@ const logger = winston.createLogger({
         new winston.transports.File( { filename: 'info.log'} )
     ]
 })
+
 //add logging to the console when not in production env
 if (process.env.NODE_ENV !== 'production') {
     logger.add(new winston.transports.Console({ 
